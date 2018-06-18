0baf06c79ab82f7dd544d6946fe317f12f00e338
jdljun <1286982153@qq.com>
2018-06-18 13:57:14 +0800

    logic update

--- src/main/java/com/se/tss/CourseArrangeMgr/logic/FeasibilityLogic.java
+++ src/main/java/com/se/tss/CourseArrangeMgr/logic/FeasibilityLogic.java
@@ -4,7 +4,7 @@ public class FeasibilityLogic {
     //在某一个教室的某一个时间段插入一门课程是否可行逻辑检验
     public String Feasibility(String teacherId, String courseId, String classRoomId,
                               int weekday,  //一周七天可上课
-                              int timePeriod) ////每天6个时间段，有的时间段是两节课，有的三节课
+                              int timePeriod) ////每天5个时间段，有的时间段是两节课，有的三节课
     {
         return ""; //可行返回空串
     }
