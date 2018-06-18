66c119a73a5e872bdeac8c9af1eeeb303c872fb6
chl9797 <chl9797@users.noreply.github.com>
2018-06-18 15:05:21 +0800

    fix no qualifying bean bug

--- src/main/java/com/se/tss/TssApplication.java
+++ src/main/java/com/se/tss/TssApplication.java
@@ -2,9 +2,15 @@ package com.se.tss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan({"com.se.tss.infomgr.service"})
+@EntityScan("com.se.tss.infomgr.model")
+@EnableJpaRepositories("com.se.tss.infomgr.model")
 @EnableJpaAuditing
 public class TssApplication {
 
