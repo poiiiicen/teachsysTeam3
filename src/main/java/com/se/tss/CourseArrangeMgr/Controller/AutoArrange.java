8b0f55cf87d565151ea8416393ec4a479a42f022
jdljun <1286982153@qq.com>
2018-06-20 21:41:48 +0800

    update structure

--- src/main/java/com/se/tss/CourseArrangeMgr/Controller/AutoArrange.java
+++ src/main/java/com/se/tss/CourseArrangeMgr/Controller/AutoArrange.java
@@ -1,19 +1,13 @@
 package com.se.tss.CourseArrangeMgr.Controller;
 
-import com.se.tss.CourseArrangeMgr.Dao.ClassRoomInfo;
 import com.se.tss.CourseArrangeMgr.Dao.ReturnDao.CourseForList;
-import com.se.tss.CourseArrangeMgr.Dao.TeacherCourseClassRoomRelation;
-import com.se.tss.CourseArrangeMgr.Service.ClassInfoService;
-import com.se.tss.CourseArrangeMgr.Service.ClassRoomInfoService;
-import com.se.tss.CourseArrangeMgr.Service.TeacherCourseClassRoomRelationService;
-import com.se.tss.CourseArrangeMgr.Service.TeacherInfoService;
+import com.se.tss.CourseArrangeMgr.logic.CourseForTeacherLogic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
@@ -22,37 +16,11 @@ import java.util.List;
 public class AutoArrange {
 
     @Autowired
-    TeacherCourseClassRoomRelationService teacherCourseClassRoomRelationService;
-    @Autowired
-    TeacherInfoService teacherInfoService;
-    @Autowired
-    ClassRoomInfoService classRoomInfoService;
-    @Autowired
-    ClassInfoService classInfoService;
+    CourseForTeacherLogic courseForTeacherLogic;
 
-    @RequestMapping(value = "/CourseArrangeList", method = RequestMethod.POST)
+    @RequestMapping(value = "/CourseForTeacherList", method = RequestMethod.GET)
     public List<CourseForList> CourseArrangeList(String name){
-        List<CourseForList> courseList = new ArrayList<CourseForList>();
-        String teacherId=teacherInfoService.findIdByName(name);
-        List<TeacherCourseClassRoomRelation> teacherCourseClassRoomRelations = teacherCourseClassRoomRelationService.findAllByTeacherid(teacherId);
-        for(TeacherCourseClassRoomRelation teacherCourseClassRoomRelation:teacherCourseClassRoomRelations){
-            String classId=teacherCourseClassRoomRelation.getCourseid();
-            String className = classInfoService.getNameById(classId);
-            String classRoomId=teacherCourseClassRoomRelation.getClassroomid();
-            int timePerid=teacherCourseClassRoomRelation.getTimeperiod();
-            int weekday=teacherCourseClassRoomRelation.getWeekday();
-            String teacherName=teacherInfoService.findNanme(teacherId);
-            ClassRoomInfo classRoomInfo=classRoomInfoService.findOneById(classRoomId);
-            String place=classRoomInfo.getPlace();
-            String roomNumber=classRoomInfo.getRoomnumber();
-            CourseForList courseForList =new CourseForList(teacherId,classId,classRoomId,weekday,timePerid,teacherName,place,roomNumber,className);
-            courseList.add(courseForList);
-        }
-        return courseList;
+        return courseForTeacherLogic.CourseArrangeList(name);
     }
 
-    @RequestMapping(value = "/CourseArrangeTable", method = RequestMethod.GET)
-    public void CourseArrangeTable(String courseName){
-
-    }
 }
