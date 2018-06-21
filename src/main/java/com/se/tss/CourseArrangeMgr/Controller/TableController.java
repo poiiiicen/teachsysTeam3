0802807d8ef5b42580be1f17471daee01442e3e5
jdljun <1286982153@qq.com>
2018-06-21 23:08:07 +0800

    增加返回3张表所有信息的接口

---
+++ src/main/java/com/se/tss/CourseArrangeMgr/Controller/TableController.java
@@ -0,0 +1,43 @@
+package com.se.tss.CourseArrangeMgr.Controller;
+
+
+import com.se.tss.CourseArrangeMgr.Dao.ClassInfo;
+import com.se.tss.CourseArrangeMgr.Dao.ClassRoomInfo;
+import com.se.tss.CourseArrangeMgr.Dao.TeacherInfo;
+import com.se.tss.CourseArrangeMgr.Service.ClassInfoService;
+import com.se.tss.CourseArrangeMgr.Service.ClassRoomInfoService;
+import com.se.tss.CourseArrangeMgr.Service.TeacherInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Controller
+@RestController
+public class TableController {
+
+    @Autowired
+    ClassRoomInfoService classRoomInfoService;
+    @Autowired
+    TeacherInfoService teacherInfoService;
+    @Autowired
+    ClassInfoService classInfoService;
+
+    @RequestMapping(value = "/AllClassRoom", method = RequestMethod.GET)
+    public List<ClassRoomInfo> AllClassRoom(){
+        return classRoomInfoService.findAll();
+    }
+
+    @RequestMapping(value = "/AllTeacher", method = RequestMethod.GET)
+    public List<TeacherInfo> AllTeacher(){
+        return teacherInfoService.findAll();
+    }
+
+    @RequestMapping(value = "/AllCourse", method = RequestMethod.GET)
+    public List<ClassInfo> AllCourse(){
+        return classInfoService.findAll();
+    }
+}
